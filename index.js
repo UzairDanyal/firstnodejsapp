3b17d0fcbb75bfb89d450668403bb34d15d0aad3
uzairDanyal <muzaird@gmail.com>
2019-12-24 22:05:11 +0500

    installed nodemon and code changed

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get('*', (req, res) => { 
-  res.send('hello world');
-});
+app.get('/', (req, res) => { 
+    res.send('hello world');
+  });
+  app.get('/students', (req, res) => { 
+    res.send('Hello students');
+  });
 
 app.listen(3000, () => {
   console.log('Express application running on localhost:3000');
