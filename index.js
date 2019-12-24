4731c8808c1f1761e3a4c2fc77c718a7460fff68
uzairDanyal <muzaird@gmail.com>
2019-12-24 22:31:48 +0500

    static data and wild card added

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => { 
-    res.send('hello world');
+    res.send('Welcome to my Node.js app');
   });
   app.get('/students', (req, res) => { 
-    res.send('Hello students');
+  
+    const listOfStudents = [
+      {id: 1,name: 'Ali'},
+      {id: 2,name: 'Ahmed'},
+      {id: 3,name: 'Danish'},
+    ]
+  
+    res.send(listOfStudents);
   });
-
-app.listen(3000, () => {
-  console.log('Express application running on localhost:3000');
-});
+  app.get('*', (req, res) => { 
+  
+  
+    res.send('Page Doesnot exists');
+  });
+  
+  app.listen(3000, () => {
+    console.log('Express application running on localhost:3000');
+  });
