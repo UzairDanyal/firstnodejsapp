dc50da39c3f3c65707175e4244275201db817e5f
uzairDanyal <muzaird@gmail.com>
2019-12-25 13:07:08 +0500

    get request added

--- index.js
+++ index.js
@@ -2,30 +2,47 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-//mongoose.connect('mongodb+srv://uzair:uzair@cluster0-dfysj.mongodb.net/mydb?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true });
-mongoose.connect('mongodb://localhost:27017/firstmongodbapp', {useNewUrlParser: true,useUnifiedTopology:true});
+mongoose.connect('mongodb://localhost:27017/firstmongodbapp', { useNewUrlParser: true, useUnifiedTopology: true });
 const Student = mongoose.model('Student', {
     name: String,
     student_id: Number,
     email: String,
     password: String,
     date_added: Date
-   });
+});
 
-// mongoose.connect('mongodb://localhost:27017/firstmongodbapp', {useNewUrlParser: true,useUnifiedTopology:true});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
+
+
 
 app.post('/signup', async (req, res) => {
     const body = req.body;
-    console.log('req.body', body);
-    res.send({
-      message: 'Success'
-    });
-    
-      });
+    console.log('req.body', body)
+    try {
+        const student = new Student(body);
+
+        const result = await student.save();
+        res.send({
+            message: 'Student signup successful'
+        });
+
+    }
+    catch (ex) {
+        console.log('ex', ex);
+        res.send({ message: 'Error' }).status(401);
+    }
+});
+
+app.get('/students', async (req, res) => {
+
+    const allStudents = await Student.find();
+    console.log('allStudents', allStudents);
+
+    res.send(allStudents);
+});
 
 app.listen(3000, () => {
     console.log('Express application running on localhost:3000');
