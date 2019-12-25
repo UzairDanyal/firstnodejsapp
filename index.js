8f80760e3bae50d0366cffb877a6198204098068
uzairDanyal <muzaird@gmail.com>
2019-12-25 12:37:09 +0500

    mongoose, parser, postman added and used

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+//mongoose.connect('mongodb+srv://uzair:uzair@cluster0-dfysj.mongodb.net/mydb?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/firstmongodbapp', {useNewUrlParser: true,useUnifiedTopology:true});
+const Student = mongoose.model('Student', {
+    name: String,
+    student_id: Number,
+    email: String,
+    password: String,
+    date_added: Date
+   });
 
-app.get('/', (req, res) => { 
-    res.send('Welcome to my Node.js app');
-  });
-  app.get('/students', (req, res) => { 
-  
-    const listOfStudents = [
-      {id: 1,name: 'Ali'},
-      {id: 2,name: 'Ahmed'},
-      {id: 3,name: 'Danish'},
-    ]
-  
-    res.send(listOfStudents);
-  });
-  app.get('*', (req, res) => { 
-  
-  
-    res.send('Page Doesnot exists');
-  });
-  
-  app.listen(3000, () => {
+// mongoose.connect('mongodb://localhost:27017/firstmongodbapp', {useNewUrlParser: true,useUnifiedTopology:true});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.post('/signup', async (req, res) => {
+    const body = req.body;
+    console.log('req.body', body);
+    res.send({
+      message: 'Success'
+    });
+    
+      });
+
+app.listen(3000, () => {
     console.log('Express application running on localhost:3000');
-  });
+});
